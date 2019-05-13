25c82962ba8e4b0897c5d8dbb5af7b4e78d82950
Wajiddat18c <waji0003@stud.kea.dk>
2019-05-13 10:16:45 +0200

    New Spring project

---
+++ Kode/efix/src/main/java/dat18c/modulopgave/efix/EfixApplication.java
@@ -0,0 +1,13 @@
+package dat18c.modulopgave.efix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EfixApplication.class, args);
+    }
+
+}
---
+++ Kode/efix/src/test/java/dat18c/modulopgave/efix/EfixApplicationTests.java
@@ -0,0 +1,16 @@
+package dat18c.modulopgave.efix;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EfixApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
