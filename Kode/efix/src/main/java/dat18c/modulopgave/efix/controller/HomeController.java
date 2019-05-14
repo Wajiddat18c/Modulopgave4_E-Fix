e08513a6beda21176ec841c78ec97416bf8f86ff
Wajiddat18c <waji0003@stud.kea.dk>
2019-05-14 11:41:51 +0200

    update homeController with buik

--- Kode/efix/src/main/java/dat18c/modulopgave/efix/controller/HomeController.java
+++ Kode/efix/src/main/java/dat18c/modulopgave/efix/controller/HomeController.java
@@ -1,7 +1,22 @@
 package dat18c.modulopgave.efix.controller;
 
+import dat18c.modulopgave.efix.service.ButikService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    ButikService butikService;
+
+    @GetMapping("/forside")
+    public String butikData(Model model){
+
+        model.addAttribute("butiks", butikService.fetchAllButiks());
+
+        return "forside";
+    }
 }
