6bbc0222835940721210b2f9d5fa21d635a76288
Wajiddat18c <waji0003@stud.kea.dk>
2019-05-14 11:19:36 +0200

    added butik class with DB variable

---
+++ Kode/efix/src/main/java/dat18c/modulopgave/efix/model/butik.java
@@ -0,0 +1,86 @@
+package dat18c.modulopgave.efix.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class butik {
+
+    @Id
+    private int idButik;
+    private String name;
+    private String adresse;
+    private int mobilNummer;
+    private String beskrivelse;
+    private String aabningstider;
+    private String email;
+
+    public butik() {
+    }
+
+    public butik(int idButik, String name, String adresse, int mobilNummer, String beskrivelse, String aabningstider, String email) {
+        this.idButik = idButik;
+        this.name = name;
+        this.adresse = adresse;
+        this.mobilNummer = mobilNummer;
+        this.beskrivelse = beskrivelse;
+        this.aabningstider = aabningstider;
+        this.email = email;
+    }
+
+    public int getIdButik() {
+        return idButik;
+    }
+
+    public void setIdButik(int idButik) {
+        this.idButik = idButik;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public int getMobilNummer() {
+        return mobilNummer;
+    }
+
+    public void setMobilNummer(int mobilNummer) {
+        this.mobilNummer = mobilNummer;
+    }
+
+    public String getBeskrivelse() {
+        return beskrivelse;
+    }
+
+    public void setBeskrivelse(String beskrivelse) {
+        this.beskrivelse = beskrivelse;
+    }
+
+    public String getAabningstider() {
+        return aabningstider;
+    }
+
+    public void setAabningstider(String aabningstider) {
+        this.aabningstider = aabningstider;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
