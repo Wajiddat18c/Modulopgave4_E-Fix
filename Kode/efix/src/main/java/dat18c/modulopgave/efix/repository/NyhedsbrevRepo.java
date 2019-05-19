5acf9c7b0abb8ec46f67f68816c2336d4f682b5e
Wajiddat18c <waji0003@stud.kea.dk>
2019-05-19 19:05:24 +0200

    Create NyhedsbrevRepo.java

---
+++ Kode/efix/src/main/java/dat18c/modulopgave/efix/repository/NyhedsbrevRepo.java
@@ -0,0 +1,31 @@
+package dat18c.modulopgave.efix.repository;
+
+import dat18c.modulopgave.efix.model.Nyhedsbrev;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class NyhedsbrevRepo {
+
+    @Autowired
+    JdbcTemplate template;
+
+    public List<Nyhedsbrev> fetchAllNyhedsbrev(){
+
+        String sql = "SELECT * FROM nyhedsbrev";
+
+        RowMapper<Nyhedsbrev> rowMapper = new BeanPropertyRowMapper<>(Nyhedsbrev.class);
+
+        return template.query(sql, rowMapper);
+    }
+    public void addNyhedsbrev(Nyhedsbrev nyhedsbrev){
+        String sql = "INSERT INTO nyhedsbrev (idNyhedsbrev, eMail) VALUES (?, ?)";
+
+        template.update(sql, nyhedsbrev.getIdNyhedsbrev(), nyhedsbrev.geteMail());
+    }
+}
