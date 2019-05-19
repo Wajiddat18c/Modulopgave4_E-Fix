6892a96f6d0eb259126a9a69b50f27d65bbd665a
Wajiddat18c <waji0003@stud.kea.dk>
2019-05-19 19:05:28 +0200

    Create NyhedsbrevService.java

---
+++ Kode/efix/src/main/java/dat18c/modulopgave/efix/service/NyhedsbrevService.java
@@ -0,0 +1,23 @@
+package dat18c.modulopgave.efix.service;
+
+import dat18c.modulopgave.efix.model.Nyhedsbrev;
+import dat18c.modulopgave.efix.repository.NyhedsbrevRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NyhedsbrevService {
+
+    @Autowired
+    NyhedsbrevRepo nyhedsbrevRepo;
+
+    public List<Nyhedsbrev> fetchAllNyhedsbrev(){
+        return nyhedsbrevRepo.fetchAllNyhedsbrev();
+    }
+
+    public void addNyhedsbrev(Nyhedsbrev nyhedsbrev){
+        nyhedsbrevRepo.addNyhedsbrev(nyhedsbrev);
+    }
+}
