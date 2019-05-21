eaa592dc5c68f851ad757c29c0c846305270eaec
Wajiddat18c <waji0003@stud.kea.dk>
2019-05-21 12:19:42 +0200

    addded frontend buy sell reparation addnyhed

--- Kode/efix/src/main/java/dat18c/modulopgave/efix/controller/FrontendController.java
+++ Kode/efix/src/main/java/dat18c/modulopgave/efix/controller/FrontendController.java
@@ -1,11 +1,14 @@
 package dat18c.modulopgave.efix.controller;
+import dat18c.modulopgave.efix.model.Nyhedsbrev;
 import dat18c.modulopgave.efix.service.ButikService;
 
+import dat18c.modulopgave.efix.service.NyhedsbrevService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FrontendController {
@@ -13,10 +16,34 @@ public class FrontendController {
     @Autowired
     ButikService butikService;
 
+    @Autowired
+    NyhedsbrevService nyhedsbrevService;
+
     @GetMapping("/")
     public String frontPage(Model model){
 
         model.addAttribute("butikker", butikService.fetchAllButiks());
         return "index";
     }
+
+    @GetMapping("/reparation")
+    public String reparation(Model model){
+        return "reparation";
+    }
+
+    @GetMapping("/buy")
+    public String buy(Model model){
+        return "buy";
+    }
+
+    @GetMapping("/sell")
+    public String sell(Model model){
+        return "sell";
+    }
+
+    @GetMapping("/addnyhed")
+    public String showAddNyhed(){
+        return "addnyhed";
+    }
+
 }
