f1a636e74336ab25d8a102b1b21d35d8ee18a505
Wajiddat18c <waji0003@stud.kea.dk>
2019-05-21 11:27:08 +0200

    Update FrontendController.java

--- Kode/efix/src/main/java/dat18c/modulopgave/efix/controller/FrontendController.java
+++ Kode/efix/src/main/java/dat18c/modulopgave/efix/controller/FrontendController.java
@@ -1,12 +1,22 @@
 package dat18c.modulopgave.efix.controller;
+import dat18c.modulopgave.efix.service.ButikService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FrontendController {
-    @RequestMapping("/")
-    public String frontPage(){
+
+    @Autowired
+    ButikService butikService;
+
+    @GetMapping("/")
+    public String frontPage(Model model){
+
+        model.addAttribute("butikker", butikService.fetchAllButiks());
         return "index";
     }
 }
