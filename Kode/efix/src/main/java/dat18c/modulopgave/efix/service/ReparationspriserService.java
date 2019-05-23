90e470c787cc1bcb20d1be64a63af2472193e3ba
Ali Al sharefi <alix4970@stud.kea.dk>
2019-05-23 12:36:55 +0200

    Lille fejl

--- Kode/efix/src/main/java/dat18c/modulopgave/efix/service/ReparationspriserService.java
+++ Kode/efix/src/main/java/dat18c/modulopgave/efix/service/ReparationspriserService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class ReprationspriserService {
+public class ReparationspriserService {
 
     @Autowired
     ReparationspriserRepo reparationspriserRepo;
